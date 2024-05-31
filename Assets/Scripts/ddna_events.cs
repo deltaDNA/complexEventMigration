09b770dcfa36e63e3813d6f06a78be2f31c1eab1
Laurie McCulloch <laurie.mcculloch@ntlworld.com>
2024-05-31 15:25:15 +0100

    deltaDNA levlUp event

--- Assets/Scripts/ddna_events.cs
+++ Assets/Scripts/ddna_events.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DeltaDNA;
 
 public class ddna_events : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        if (DDNA.Instance.HasStarted)
+        {
+            Debug.Log("DeltaDNA SDK is Running");
+            return;
+        }
         
+        // Configure the SDK
+        DDNA.Instance.SetLoggingLevel(DeltaDNA.Logger.Level.DEBUG);
+        DDNA.Instance.SetPiplConsent(true,true);
+        DDNA.Instance.StartSDK("Complex-Event-User");        
     }
 
     public void Home_Button_Clicked()
@@ -21,6 +31,25 @@ public class ddna_events : MonoBehaviour
     {
         // Send event button clicked
         // =========================
-        Debug.Log($"Send Event Button Clicked on DDNA Events page");        
+        Debug.Log($"Send Event Button Clicked on DDNA Events page");
+
+
+        // Record a deltaDNA levelUp event, with reward object containing
+        // a productReceived Object
+        var gameEvent = new GameEvent("levelUp")
+            .AddParam("goldBalance", 5400)
+            .AddParam("livesBalance", 3)
+            .AddParam("levelUpName", "Level 5")
+            .AddParam("userLevel",5)
+            .AddParam("reward", new Params()
+                .AddParam("rewardName", "Level Up Reward")
+                .AddParam("rewardProducts", new Product()
+                    .AddVirtualCurrency("Diamonds", "GRIND", 20)
+                    .AddItem("MultiStrike", "Power Up", 1)
+            )
+        );
+
+        DDNA.Instance.RecordEvent(gameEvent);
+        DDNA.Instance.Upload();
     }
 }
