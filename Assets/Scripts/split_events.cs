23d67b611e6f3f8087f0bf3e4a596d0d01c259e5
Laurie McCulloch <laurie.mcculloch@ntlworld.com>
2024-05-31 17:32:12 +0100

    Added Unity Analytics split levelUpFlattened and transaction events (2)

--- Assets/Scripts/split_events.cs
+++ Assets/Scripts/split_events.cs
@@ -1,13 +1,45 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.Services.Core;
+using Unity.Services.Core.Environments;
+using Unity.Services.Analytics;
 
 public class split_events : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
-        
+        // Check to see if Unity Services is already initialized
+        // =====================================================
+        if(UnityServices.State == ServicesInitializationState.Initialized)
+        {
+            Debug.Log($"Unity Analytics is already running");
+            return;
+        }
+
+        // Set Analytics environment to 'development'
+        //===========================================
+        var options = new InitializationOptions();
+        options.SetEnvironmentName("development");
+        await UnityServices.InitializeAsync(options);
+
+
+        // Set custom userID
+        // ==================
+        UnityServices.ExternalUserId = "Complex-Unity-Event-User";
+
+
+        // Initialize UGS
+        // ==============
+        await UnityServices.InitializeAsync();
+
+
+        // Start the Unity Analytics service
+        // =================================
+        AnalyticsService.Instance.StartDataCollection();
     }
+
 
     public void Home_Button_Clicked()
     {
@@ -17,10 +49,66 @@ public class split_events : MonoBehaviour
         SceneManager.LoadScene("0-main", LoadSceneMode.Single);
     }
 
+
     public void Send_Button_Clicked()
     {
         // Send event button clicked
         // =========================
         Debug.Log($"Send Event Button Clicked on Split Events page");
+
+        // Generate a transactionID to link the levelUp and transaction events
+        // back together again in any Analytics SQL queries
+        // ===================================================================
+        string transactionId = string.Concat(
+            AnalyticsService.Instance.GetAnalyticsUserID(),
+            "-",
+            DateTime.Now.Ticks
+            );
+        Debug.Log(transactionId);
+
+        // Send flat levelUp event containing the level details
+        // ====================================================
+        CustomEvent levelUpEvent = new CustomEvent("levelUpFlattened")
+        {
+            { "goldBalance", 5600 },
+            { "livesBalance", 3 },
+            { "levelUpName", "Level 6" },
+            { "userLevel", 6 },
+            { "transactionID", transactionId},
+            { "sdkVersion", Unity.Services.Analytics.SdkVersion.SDK_VERSION }
+            
+        };
+       
+        AnalyticsService.Instance.RecordEvent(levelUpEvent);
+
+        // Send a transaction event containing the details of the reward
+        // =============================================================
+        TransactionEvent transaction = new TransactionEvent
+        {
+            TransactionId = transactionId,
+            TransactionName = "Level Up Reward",
+            TransactionType = TransactionType.TRADE,                        
+        };
+
+        transaction.ReceivedItems.Add(new TransactionItem
+        {
+            ItemName = "Poweball",
+            ItemType = "Power Up",
+            ItemAmount = 1
+        });
+
+        transaction.ReceivedVirtualCurrencies.Add(new TransactionVirtualCurrency
+        {
+            VirtualCurrencyName = "Diamonds",
+            VirtualCurrencyType =  VirtualCurrencyType.GRIND,
+            VirtualCurrencyAmount = 20
+        });
+
+        AnalyticsService.Instance.RecordEvent(transaction);
+
+        // Upload the events
+        // =================
+        AnalyticsService.Instance.Flush();
+
     }
 }
