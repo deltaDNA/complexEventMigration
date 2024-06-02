5e86b1ddb2dba32d28ce4aa5cbe96e98737e2766
Laurie McCulloch <laurie.mcculloch@ntlworld.com>
2024-06-02 08:36:03 +0100

    Added Unity Analytics stringified productsReceived object (3)

--- Assets/Scripts/string_events.cs
+++ Assets/Scripts/string_events.cs
@@ -1,12 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.Services.Core;
+using Unity.Services.Core.Environments;
+using Unity.Services.Analytics;
+using Newtonsoft.Json;
 
 public class string_events : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
-        
+        // Check to see if Unity Services is already initialized
+        // =====================================================
+        if (UnityServices.State == ServicesInitializationState.Initialized)
+        {
+            Debug.Log($"Unity Analytics is already running");
+            return;
+        }
+
+        // Set Analytics environment to 'development'
+        //===========================================
+        var options = new InitializationOptions();
+        options.SetEnvironmentName("development");
+        await UnityServices.InitializeAsync(options);
+
+
+        // Set custom userID
+        // ==================
+        UnityServices.ExternalUserId = "Complex-Unity-Event-User";
+
+
+        // Initialize UGS
+        // ==============
+        await UnityServices.InitializeAsync();
+
+
+        // Start the Unity Analytics service
+        // =================================
+        AnalyticsService.Instance.StartDataCollection();
     }
 
     public void Home_Button_Clicked()
@@ -22,5 +55,79 @@ public class string_events : MonoBehaviour
         // Send event button clicked
         // =========================
         Debug.Log($"Send Event Button Clicked on String Reward Events page");
+
+
+        // Create Rewards String
+        TransactionVirtualCurrency v = new TransactionVirtualCurrency()
+        {
+            VirtualCurrencyName = "Diamonds",
+            VirtualCurrencyType = VirtualCurrencyType.GRIND,
+            VirtualCurrencyAmount = 20
+        };
+
+        // Create Rewards String
+        TransactionVirtualCurrency c = new TransactionVirtualCurrency()
+        {
+            VirtualCurrencyName = "Carrots",
+            VirtualCurrencyType = VirtualCurrencyType.GRIND,
+            VirtualCurrencyAmount = 20
+        };
+
+        // Create Rewards String
+        TransactionItem i = new TransactionItem()
+        {
+            ItemName = "Powerball",
+            ItemType = "Power Up",
+            ItemAmount = 1
+        };
+
+        Products productsRecieved_s = new Products();
+        productsRecieved_s.virtualCurrencies.Add(v);
+        productsRecieved_s.virtualCurrencies.Add(c);
+        productsRecieved_s.items.Add(i);
+    
+        
+        Debug.Log($"Transaction Products {productsRecieved_s.ToString()}");
+
+
+        // Send levelUp event containing a reward product object as a string
+        // =================================================================
+        CustomEvent levelUpEvent = new CustomEvent("levelUpStringified")
+        {
+            { "rewardName", "LevelUp Reward"},
+            { "goldBalance", 5600 },
+            { "livesBalance", 3 },
+            { "levelUpName", "Level 6" },
+            { "userLevel", 6 },            
+            { "sdkVersion", Unity.Services.Analytics.SdkVersion.SDK_VERSION },
+            { "productsReceived_s", productsRecieved_s.ToString() }
+
+        };
+
+        AnalyticsService.Instance.RecordEvent(levelUpEvent);
+        AnalyticsService.Instance.Flush();
+    }
+
+    [System.Serializable]
+    public class Products
+    {
+        public TransactionRealCurrency realCurrency;
+        public List<TransactionVirtualCurrency> virtualCurrencies;
+        public List<TransactionItem> items;
+
+        public Products()
+        {
+            virtualCurrencies = new List<TransactionVirtualCurrency>();
+            items = new List<TransactionItem>();
+            
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.None, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+        }
     }
 }
